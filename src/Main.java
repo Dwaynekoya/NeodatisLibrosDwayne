8cc3ea3280911bcdc91db259ea2d4a569de04417
Dwaynekoya <101224368+Dwaynekoya@users.noreply.github.com>
2024-01-22 13:52:53 +0100

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //USER LOGIN
+
+        //CONEXION BBDD
+
+        //INSERTAR DATOS PRUEBA
+
+        //VISUALIZAR
+
     }
 }
--- src/modelo/Libro.java
+++ src/modelo/Libro.java
@@ -1,5 +1,71 @@
 package modelo;
 
+import java.util.Date;
+
 public class Libro {
 
+    private String nombre;
+    private String genero;
+    private String sinopsis;
+    private Date fecha_lanzamiento;
+    private Autor autor;
+
+    public Libro(String nombre, String genero, String sinopsis, Date fecha_lanzamiento, Autor autor) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.sinopsis = sinopsis;
+        this.fecha_lanzamiento = fecha_lanzamiento;
+        this.autor = autor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+    public Date getFecha_lanzamiento() {
+        return fecha_lanzamiento;
+    }
+
+    public void setFecha_lanzamiento(Date fecha_lanzamiento) {
+        this.fecha_lanzamiento = fecha_lanzamiento;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "nombre='" + nombre + '\'' +
+                ", genero='" + genero + '\'' +
+                ", sinopsis='" + sinopsis + '\'' +
+                ", fecha_lanzamiento=" + fecha_lanzamiento +
+                ", autor=" + autor +
+                '}';
+    }
 }
